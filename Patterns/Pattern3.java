ef1b7c0d11faa4babfe862e903b9a70b1e456902
Pratham-2105 <prathamsri.21@gmail.com>
2025-06-09 00:15:55 +0530

    Solved Problem-3

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter num: ");
+        int num = scan.nextInt();
+
+        int i;
+        for (i = 1; i <= num; i++) {
+            int j;
+            for (j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
