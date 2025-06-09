1b6067b97b4c6510db5999c1aac56f711f2aa41f
Pratham Srivastava <prathamsri.21@gmail.com>
2025-06-10 01:59:14 +0530

    Changed the Space

--- Patterns/Pattern13.java
+++ Patterns/Pattern13.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Pattern13 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num = 5;
+        System.out.print("Enter num: ");
+        int num = scan.nextInt();
         scan.close();
 
         int number = 1;
         for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(number);
+                System.out.print(number + " ");
                 number++;
-
-                if (j != i) {
-                    System.out.print(" ");
-                }
+                /*
+                 * if (j != i) {
+                 * System.out.print(" ");
+                 * }
+                 */
             }
             System.out.println();
         }
