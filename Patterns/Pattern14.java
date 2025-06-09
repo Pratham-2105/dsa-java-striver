cac2c3441f66a694671fe6cc63fea653189b34c4
Pratham Srivastava <prathamsri.21@gmail.com>
2025-06-10 01:55:59 +0530

    Solved Pattern-14

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter num: ");
+        int num = scan.nextInt();
+        scan.close();
+
+        for (int i = 1; i <= num; i++) {
+            char start = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(start);
+                start++;
+            }
+            System.out.println();
+        }
+
+    }
+}
