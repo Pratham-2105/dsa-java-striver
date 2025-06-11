52129f97ae800ecb72a30c8484ad1496ebde62f4
Pratham Srivastava <prathamsri.21@gmail.com>
2025-06-12 00:58:06 +0530

    Completed Iterators

---
+++ Collections/Iterators/List_Iterator.java
@@ -0,0 +1,28 @@
+package Collections.Iterators;
+
+import java.util.*;
+
+public class List_Iterator {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(12);
+        arr.add(24);
+        arr.add(15);
+        arr.add(20);
+        System.out.println(arr);
+
+        /*
+         * for (var num : arr) {
+         * System.out.println(num);
+         * }
+         * System.out.println();
+         */
+
+        Iterator<Integer> it = arr.iterator();
+        while (it.hasNext()) {
+            Integer num = it.next();
+            System.out.println(num);
+        }
+    }
+}
